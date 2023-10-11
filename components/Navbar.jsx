bda259cfda8bab8020825a5540c9dc53d4408db4
gablon29 <gabriellondero40@gmail.com>
2023-10-11 19:31:35 -0300

    creando las rutas de about y home

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Este es el Navbar</h1>
+      <ul>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
